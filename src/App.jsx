f5f89f9402a7a172b2cb2c6daa706a95397d8bd2
Paloma <paloma.sanchezc@icloud.com>
2025-09-10 15:41:31 +0200

    ssd

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Mask from './Mask';
+import Vinyle from './vinyle';
+import Vinyle_cup from './Vinyle_cup';
+import { CubeCamera } from '@react-three/drei';
+
+function App() {
+  return (
+    <Vinyle_cup />
+  );
+}
+
+export default App;
